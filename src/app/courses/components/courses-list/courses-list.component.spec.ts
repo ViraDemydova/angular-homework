ceed380b053b260dc1afd2e21235f8756e62cea9
Vira Demydova <Vira_Demydova@epam.com>
2018-07-09 18:47:25 +0300

    work on unit tests

--- src/app/courses/components/courses-list/courses-list.component.spec.ts
+++ src/app/courses/components/courses-list/courses-list.component.spec.ts
@@ -2,19 +2,38 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesListComponent } from './courses-list.component';
 import {CoursesListItemComponent} from './courses-list-item/courses-list-item.component';
+import {CoursesListItemService} from "../../services/courses-list-item.service";
+
+// stub courseService для тестирования компонента
+const courseServiceStub = {
+  id: 1,
+  title: 'Video Course ',
+  createDate: '05.29.2018',
+  duration: '1h 28min',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+};
+
 
 describe('CoursesListComponent', () => {
   let component: CoursesListComponent;
   let fixture: ComponentFixture<CoursesListComponent>;
+  let courseService: CoursesListItemService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         CoursesListComponent,
         CoursesListItemComponent
+      ],
+      providers: [
+        CoursesListComponent,
+        CoursesListItemComponent,
+        { provide: CoursesListItemService, useValue: courseServiceStub }
       ]
-    })
-    .compileComponents();
+    });
+
+    component = TestBed.get(CoursesListComponent);
+    courseService = TestBed.get(CoursesListItemService);
   }));
 
   beforeEach(() => {
@@ -23,13 +42,8 @@ describe('CoursesListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show id after Angular calls ngOnInit', () => {
+    //component.ngOnInit();
+    //expect(courseServiceStub.id).toContain(1);
   });
-
-  it('Courses-list component should contain "LOAD MORE:', () => {
-    const bannerElement: HTMLElement = fixture.nativeElement;
-    expect(bannerElement.textContent).toContain('LOAD MORE');
-  });
-
 });
