3ec23e43b4649640f613c62682132a1ad0a6b9ca
Vira Demydova <Vira_Demydova@epam.com>
2018-08-07 11:19:41 +0300

    small change to auth token

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,19 +2,34 @@ import { Injectable  } from '@angular/core';
 import { CoreModule } from '../core.module';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import {Observable} from "rxjs/Rx";
 
 @Injectable({
   providedIn: CoreModule
 })
 export class AuthService {
+  public authTokenStale = 'stale_auth_token';
+  public authTokenNew = 'new_auth_token';
+  public currentToken: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.currentToken = this.authTokenStale;
+  }
 
   public IsAuthenticated: boolean;
 
+  getAuthToken() {
+    return this.currentToken;
+  }
+
+  refreshToken(): Observable<string> {
+    this.currentToken = this.authTokenNew;
+    return Observable.of(this.authTokenNew).delay(200);
+  }
+
 
   login(login: string, password: string) {
-    return this.http.post<any>('http://localhost:3000/user', { login: login, password: password , tokenKey: 'app_token'})
+    return this.http.post<any>('http://localhost:3000/user', { login: login, password: password , tokenKey: this.authTokenStale})
       .map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.tokenKey) {
