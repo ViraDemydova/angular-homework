52095f358a738906a0424dd8eb9dc53438abf021
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-08-15 17:35:49 +0300

    5

--- src/app/core/guards/canActivateGuard.ts
+++ src/app/core/guards/canActivateGuard.ts
@@ -1,45 +1,40 @@
 ﻿import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { CanActivate } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { CoreModule } from '../core.module';
 import { AuthService } from '../services/auth.service';
 import { SharedService } from '../services/shared.service';
-import { Observable } from 'rxjs';
-import {  map } from 'rxjs/operators';
-import { catchError } from 'rxjs/operators';
-import { CoreModule } from '../core.module';
 
 @Injectable({
   providedIn: CoreModule
 })
 export class CanActivateGuard implements CanActivate {
-  constructor(private serviceAuth: AuthService,
-              private router: Router,
-              private sharedService: SharedService
+  constructor(
+    private serviceAuth: AuthService,
+    private sharedService: SharedService
   ) {}
 
   IsAuth() {
-    console.log('IsAuthenticated from guard:', this.serviceAuth.isAuthenticated());
+    console.log(
+      'IsAuthenticated from guard:',
+      this.serviceAuth.isAuthenticated()
+    );
     return this.serviceAuth.isAuthenticated();
   }
 
-  // canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-   // if (this.IsAuth()) {
-    //  return true;
-    // } else {
-    //  this.router.navigate(['/login']);
-   // }
-  // }
-
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.sharedService.user$
-        .pipe(map(user => {
-          if (user) {
-            return true;
-          }
-        }),
-         catchError(() => {
-          return Observable.of(false);
-        }));
-    }
+  canActivate(): Observable<boolean> {
+    return this.sharedService.user$.pipe(
+      map(user => {
+        if (user) {
+          return true;
+        }
+      }),
+      catchError(() => {
+        return Observable.of(false);
+      })
+    );
+  }
 }
-
-
